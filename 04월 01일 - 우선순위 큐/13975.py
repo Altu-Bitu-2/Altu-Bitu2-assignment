f8bac4dc2f91c3636c91f63c7c21603c9ec6584a
HYEMIN-HEO <97100404+HYEMIN-HEO@users.noreply.github.com>
2022-04-14 02:14:28 +0900

    [우선순위 큐] 4월 14일 - Update

--- 04월 01일 - 우선순위 큐/13975.py
+++ 04월 01일 - 우선순위 큐/13975.py
@@ -4,12 +4,10 @@ input=sys.stdin.readline
 t=int(input())
 for _ in range(t):
     k=int(input())
-    temp=list(map(int,input().split()))
-    chap=[]
+    chap=list(map(int,input().split()))
+    hq.heapify(chap)
     sum=0
-    for i in temp:
-        hq.heappush(chap,i)
-
+    
     #파일이 하나로 합쳐질때까지 반복    
     while len(chap)!=1:
         cost=hq.heappop(chap)+hq.heappop(chap)
--- 04월 01일 - 우선순위 큐/14235.py
+++ 04월 01일 - 우선순위 큐/14235.py
@@ -5,14 +5,12 @@ n=int(input()) #방문 횟수
 gift=[]
 for _ in range(n):
     line=list(map(int,input().split()))
-    a=line[0]
     #선물 가치 저장한 최대힙 생성
     for i in line[1::]:
         hq.heappush(gift,-i)
         
-    if a==0:
+    if line[0]==0:
         if len(gift)==0:
             print(-1)
         else:
             print(-hq.heappop(gift))
-        
